0d34481012517b630c6f26d541e923cb72e07525
Admin <admin@MacBook-Pro-cua-Admin.local>
2024-12-03 21:03:48 +0700

    initial

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return <div>page</div>;
+};
+
+export default page;
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return <div>Dashboard</div>;
+};
+
+export default page;
