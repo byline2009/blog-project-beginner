7545a62e5864eed09b44fc99335735ccaccfb4a3
Admin <admin@MacBook-Pro-cua-Admin.local>
2024-12-17 21:06:21 +0700

    update layout

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,9 +1,28 @@
 
 import React from "react";
-
-const Page = () => {
-    return <>
-        <h1>Blog Detail</h1>
-    </>
+export default async function Page({
+    params,
+}: {
+    params: Promise<{ slug: string }>
+}) {
+    
+    const { slug } = await params
+    const res = await fetch(`https://houze-portal-api.houze.io/portal/blogs/${slug}`)
+    const blog = await res.json();
+    return (
+        <article>
+            <h1>{blog.title}</h1>
+            <p>{blog.content}</p>
+        </article>
+    )
 }
-export default Page;
+
+export async function generateStaticParams() {
+    const blogs = await fetch('https://houze-portal-api.houze.io/portal/blogs?is_hero=false&is_most_read=false&limit=1000&offset=0&ordering=-publish_time&section_id=1', {
+        cache: 'force-cache',
+    }).then((res) => res.json())
+
+    return blogs.results.map((blog: any) => ({
+        slug: String(blog.slug),
+    }))
+}
